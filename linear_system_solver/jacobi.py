Updated the diagonal calculation for improved performances, fixed some typos
Co-authored-by: Riccardo-Ghilotti <r.ghilotti@campus.unimib.it>

linear_system_solver/jacobi.py
@@ -3,7 +3,7 @@ import timeit
 import warnings
 from util import is_diagonally_dominant, any_stopping_criteria_met
 
-def jacobi_solver(A, b, tollerance, max_iterations, initial_x):
+def jacobi_solver(A, b, tolerance, max_iterations, initial_x):
     
     start_time = timeit.default_timer()
 
@@ -11,24 +11,21 @@ def jacobi_solver(A, b, tollerance, max_iterations, initial_x):
     if not is_diagonally_dominant(A):
         warnings.warn("Jacobi method may diverge since A is not diagonally dominant.")
 
-    # Get the diagonal matrix of A
-    D = np.diag(np.diag(A))
-
     # Get the inverse diagonal matrix of A
-    D_inv = np.linalg.inv(D) 
+    D_inv = np.diag(1 / np.diag(A))
     
     # Set up variables 
     iterations = 0
-    x_old = initial_x
+    x_old = np.copy(initial_x)
     x_new = np.copy(x_old)
 
     # needed to get at least one iteration, alternatively iterate once here or set x_new = x_old + 1
-    abs_error = tollerance + 1
-    rel_error = tollerance + 1
+    abs_error = tolerance + 1
+    rel_error = tolerance + 1
 
 
     # iterate until any stopping critieria is met
-    while not any_stopping_criteria_met(iterations, rel_error, max_iterations, tollerance):
+    while not any_stopping_criteria_met(iterations, rel_error, max_iterations, tolerance):
         x_old = np.copy(x_new)
 
         # calculate the residue vector

linear_system_solver/util.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-def any_stopping_criteria_met(iterations, rel_error, max_iterations, tollerance):
+def any_stopping_criteria_met(iterations, rel_error, max_iterations, tolerance):
     """
     Checks whether the stopping criteria are met.
 
     Parameters:
-    - iterations: number of current iteration
-    - rel_error: current relative error
-    -   
+    - iterations: Current number of iterations
+    - rel_error: Relative error of current iteration
+    - max_iterations: Maximum number of allowed iterations
+    - tolerance: Convergence tolerance
+
 
     Returns:
     - True if the matrix is diagonally dominant for the given axis,
@@ -15,7 +17,7 @@ def any_stopping_criteria_met(iterations, rel_error, max_iterations, tollerance)
 
     """
 
-    if((iterations < max_iterations) and (rel_error > tollerance)):
+    if((iterations < max_iterations) and (rel_error > tolerance)):
         return False
     return True
 
@@ -24,11 +26,11 @@ def is_diagonally_dominant(matrix, axis=1):
     Checks whether the given matrix is diagonally dominant.
 
     Parameters:
-    - matrix: given matrix
-    - axis: 1 for rows (default), 0 for columns 
+    - matrix: Given matrix, passed as a square NumPy array.
+    - axis: 1 for row dominance (default), 0 for column dominance.
 
     Returns:
-    - True if the matrix is diagonally dominant for the given axis,
+    - True if the matrix is diagonally dominant along the specified axis,
       False otherwise.
 
     """
@@ -37,8 +39,8 @@ def is_diagonally_dominant(matrix, axis=1):
     abs_matrix = np.abs(matrix)
     
     # Calculate the Diagonal values of the matrix
-    D = abs_matrix.diagonal()
-
+    D = np.diag(abs_matrix)
+    
     # Sum the elements of the row, subtracting the diagonal element
     # If axis is 0, then do the same operation but on columns instead
     S = abs_matrix.sum(axis) - D